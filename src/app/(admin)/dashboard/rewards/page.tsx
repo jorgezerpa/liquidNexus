565752342bd8a2ef7fbc5009685cf411cc6df565
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-02-03 12:43:49 -0400

    new color scheme for Rewards

--- src/app/(admin)/dashboard/rewards/page.tsx
+++ src/app/(admin)/dashboard/rewards/page.tsx
@@ -89,6 +89,30 @@ const recentActivity = [
     info1: "Storage limit: 2GB",
     info2: "",
   },
+  {
+    activity: "Contributed 5GB bandwidth to Node X",
+    time: "30s ago",
+    info1: "Reward amount: 0.04 $NEXUS",
+    info2: "Paid to: 0x334...234",
+  },
+  {
+    activity: "Contributed 5GB bandwidth to hyperliquid nodes syncronization",
+    time: "47s ago",
+    info1: "Reward amount: 0.23 $NEXUS",
+    info2: "Paid to: 0x334...234",
+  },
+  {
+    activity: "Disable Google Cloud Storage API",
+    time: "2min ago",
+    info1: "Connected since: 01/01/2025",
+    info2: "Generated amount API contributions: 300$",
+  },
+  {
+    activity: "Enable AWS S3 API for storage contributions",
+    time: "wednesday",
+    info1: "Storage limit: 2GB",
+    info2: "",
+  },
 ]
 
 const rewardHistory = [
@@ -131,22 +155,22 @@ export default function Rewards() {
     <div className="py-5 px-5">
       
       <div className="flex justify-between">
-        <div className="flex-shrink-0 w-[69%] bg-gray-100 rounded-xl p-4">
-          <p className="text-5xl font-bold">$1565.55</p>
-          <p className="text-xl text-green-600 mb-10">+2% increase per day (last 30d)</p>
-          <p className="text-xl text-gray-700 font-bold mb-2">Daily revenue</p>          
+        <div className="flex-shrink-0 w-[69%] bg-black bg-opacity-40 shadow-sm shadow-white rounded-xl p-4">
+          <p className="text-5xl font-bold text-title">$1565.55</p>
+          <p className="text-xl text-green-300 mb-10">+2% increase per day (last 30d)</p>
+          <p className="text-xl text-gray-200 font-bold mb-2">Daily revenue</p>          
           <LineChart data={dataLineChart} lines={[{ dataKey:"amount", stroke:"#8884d8" }]} hasLegend={false} />
         </div>
-        <div className="w-[29%] bg-gray-50 h-[500px] overflow-y-scroll rounded-xl px-3">
-          <p className="text-2xl font-bold">Recent Activity</p>
+        <div className="w-[29%] h-[500px] vertical-scroll rounded-xl px-3 bg-black bg-opacity-40 shadow-sm shadow-white py-3">
+          <p className="text-2xl font-bold text-gray-200">Recent Activity</p>
           <div className="h-5"></div>
           {
             recentActivity.map((activity, i)=>{
               return(
-                <div className="flex-shrink-0 w-[100%] bg-gray-100 rounded-xl px-4 py-1 mb-3">
+                <div className="flex-shrink-0 w-[100%] bg-white bg-opacity-10 rounded-xl px-4 py-1 mb-3">
                   <div className="flex  justify-between mb-4">
-                    <p className="text-lg text-gray-700 font-bold">{activity.activity}</p>          
-                    <p className="text-xl text-gray-700 ">{ activity.time }</p>          
+                    <p className="text-lg text-gray-100 font-bold">{activity.activity}</p>          
+                    <p className="text-sm  flex-shrink-0">{ activity.time }</p>          
                   </div>
                 </div>
               )
@@ -159,21 +183,21 @@ export default function Rewards() {
 
       <div className="flex justify-between">
         <div className="w-[100%]">
-          <p className="text-2xl font-bold">Reward History</p>
+          <p className="text-2xl font-bold text-gray-100">Reward History</p>
           <div className="h-5"></div>
           {
             rewardHistory.map((activity, i)=>{
               return(
-                <div className="flex-shrink-0 w-[100%] bg-gray-100 rounded-xl px-4 py-1 mb-3">
+                <div className="flex-shrink-0 w-[100%] bg-white bg-opacity-10 rounded-xl px-4 py-1 mb-3">
                   <div className="flex  justify-between mb-2">
-                    <p className="text-lg text-gray-700 font-bold">Reward: {activity.reward}</p>          
-                    <p className="text-xl text-gray-700 ">{ activity.time }</p>          
+                    <p className="text-lg text-gray-100 font-bold">Reward: {activity.reward}</p>          
+                    <p className="text-xl text-gray-200 ">{ activity.time }</p>          
                   </div>
                   <div className="flex  justify-between">
-                    <p className="text-lg text-gray-700">paid to: {activity.paidTo}</p>          
+                    <p className="text-lg">paid to: {activity.paidTo}</p>          
                   </div>
                   <div className="flex  justify-between">
-                    <p className="text-lg text-gray-700">Contribution: {activity.activity}</p>          
+                    <p className="text-lg">Contribution: {activity.activity}</p>          
                   </div>
                 </div>
               )
